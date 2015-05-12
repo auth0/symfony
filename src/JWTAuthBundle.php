bd72ac18ab53f34d90af00c64f3b0bc2c9421460
German Lena <german.lena@gmail.com>
2015-05-12 12:18:47 -0300

    SDK Client headers spec compliant #6

--- src/JWTAuthBundle.php
+++ src/JWTAuthBundle.php
@@ -4,9 +4,17 @@ namespace Auth0\JWTAuthBundle;
 
 use Auth0\JWTAuthBundle\DependencyInjection\Auth0Extension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Auth0\SDK\API\ApiClient;
+use Symfony\Component\HttpKernel\Kernel;
 
 class JWTAuthBundle extends Bundle
 {
+	const SDK_VERSION = "1.2.1";
+
+	public function __construct() {
+		ApiClient::addHeaderInfoMeta('Symfony:'.Kernel::VERSION);
+		ApiClient::addHeaderInfoMeta('SDK:'.self::SDK_VERSION);
+	}
 
     public function getAlias()
     {
