f67ac28514fda808880c64371281b98e6d1b4d6a
James Rickard <james@frodosghost.com>
2016-01-29 08:10:27 -0500

    Updated tests to work with the Symfony 3.0 changes.

--- Tests/Security/JWTAuthenticatorTest.php
+++ Tests/Security/JWTAuthenticatorTest.php
@@ -4,21 +4,29 @@ namespace Auth0\JWTAuthBundle\Tests\Security;
 
 use Auth0\JWTAuthBundle\Security\Auth0Service;
 use Auth0\JWTAuthBundle\Security\JWTAuthenticator;
-use Symfony\Component\HttpFoundation\Request;
 
 class JWTAuthenticatorTest extends \PHPUnit_Framework_TestCase
 {
     public function testTokenCreation()
     {
-        $authenticator = new JWTAuthenticator(new Auth0Service('client_id', null, null));
+        $mockAuth0 = $this->getMockBuilder('Auth0\JWTAuthBundle\Security\Auth0Service')
+          ->disableOriginalConstructor()
+          ->getMock();
+
+        $mockAuth0->expects($this->once())
+            ->method('decodeJWT')
+            ->will($this->returnValue(new \stdClass()));
+
+        $authenticator = new JWTAuthenticator($mockAuth0);
         $providerKey = 'providerKey';
 
         //generated with http://jwt.io/
         $JWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2RvbWFpbi5jb20vIiwic3ViIjoiYXV0aDB8MDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImF1ZCI6ImNsaWVudF9pZCIsImV4cCI6MTQyMjQ0MDI3MSwiaWF0IjoxNDIyNDA0MjcxfQ.xSuCAetwfHpCWhE_5NqTrwHq0eQ7CVffQwgSqTHwwrY';
 
-        $request = $this->getMock('Request');
-        $request->headers = $this->getMock('ParameterBag');
-        $request->request
+        $request = $this->getMock('Symfony\Component\HttpFoundation\Request');
+        $request->headers = $this->getMock('Symfony\Component\HttpFoundation\ParameterBag');
+
+        $request->headers
             ->expects($this->atLeastOnce())
             ->method('get')
             ->with('Authorization')
@@ -30,4 +38,5 @@ class JWTAuthenticatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($providerKey,$token->getProviderKey());
         $this->assertEquals($providerKey,$token->getProviderKey());
     }
+
 }
