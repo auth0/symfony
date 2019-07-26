deba3132cb25c2c91846b6893f0d555f925aa352
Vincent Falduto <vincent.falduto@gmail.com>
2019-07-26 15:22:26 +0200

    Fixed deprecated root call
    Fixed deprecated call 
    
     The "Symfony\Component\Config\Definition\Builder\TreeBuilder::root()" method called for the "jwt_auth" configuration is deprecated since Symfony 4.3, pass the root name to the constructor instead.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('jwt_auth');
+        $treeBuilder = new TreeBuilder('jwt_auth');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
