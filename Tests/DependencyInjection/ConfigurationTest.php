862587efabf038e9668bb7b0aba61b5ebd34df9a
Antzo Lorén <antzo90@gmail.com>
2019-12-10 17:53:55 +0100

    Configuration authorized_issuer string or array compatibility (#89)

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Auth0\JWTAuthBundle\Tests\DependencyInjection;
+
+use Auth0\JWTAuthBundle\DependencyInjection\JWTAuthExtension;
+use Auth0\JWTAuthBundle\JWTAuthBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var JWTAuthBundle  */
+    private $extension;
+    /** @var ContainerBuilder  */
+    private $container;
+    /** @var string  */
+    private $rootNode;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->extension = new JWTAuthExtension();
+        $this->container = new ContainerBuilder();
+        $this->rootNode = 'jwt_auth';
+    }
+
+    public function testGetConfigWhenMultipleApiIdentifier()
+    {
+        $configs = [
+            'api_identifier_array' => [
+                'test identifier1',
+                'test identifier2'
+            ],
+            'api_client_id' => 'test client id'
+        ];
+
+        $this->extension->load([$configs], $this->container);
+
+        $this->assertTrue($this->container->hasParameter($this->rootNode . '.api_identifier'));
+        $this->assertEquals(
+            [
+                'test identifier1',
+                'test identifier2',
+                'test client id'
+            ],
+            $this->container->getParameter($this->rootNode . '.api_identifier')
+        );
+    }
+
+    public function testGetConfigWhenSingleApiIdentifier()
+    {
+        $configs = [
+            'api_identifier' => 'test identifier'
+        ];
+
+        $this->extension->load([$configs], $this->container);
+
+        $this->assertTrue($this->container->hasParameter($this->rootNode . '.api_identifier'));
+        $this->assertEquals('test identifier', $this->container->getParameter($this->rootNode . '.api_identifier'));
+    }
+
+    public function testGetConfigWhenMultipleAuthorizedIssuer()
+    {
+        $configs = [
+            'authorized_issuer' => [
+                'test authorized issuer1',
+                'test authorized issuer2'
+            ]
+        ];
+
+        $this->extension->load([$configs], $this->container);
+
+        $this->assertTrue($this->container->hasParameter($this->rootNode . '.authorized_issuer'));
+        $this->assertEquals(
+            [
+                'test authorized issuer1',
+                'test authorized issuer2'
+            ],
+            $this->container->getParameter($this->rootNode . '.authorized_issuer')
+        );
+    }
+
+    public function testGetConfigWhenSingleAuthorizedIssuer()
+    {
+        $configs = [
+            'authorized_issuer' => 'test authorized issuer'
+        ];
+
+        $this->extension->load([$configs], $this->container);
+
+        $this->assertTrue($this->container->hasParameter($this->rootNode . '.authorized_issuer'));
+        $this->assertEquals('test authorized issuer', $this->container->getParameter($this->rootNode . '.authorized_issuer'));
+    }
+}
