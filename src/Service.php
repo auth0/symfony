2dd33ad9680c6363ee029fe9298fde3d97e545ec
Evan Sims <evan.sims@okta.com>
2022-12-15 15:14:54 -0600

    Bump to 5.0.1; add network telemetry metrics

--- src/Service.php
+++ src/Service.php
@@ -6,12 +6,16 @@ namespace Auth0\Symfony;
 
 use Auth0\SDK\Auth0;
 use Auth0\SDK\Configuration\SdkConfiguration;
+use Auth0\SDK\Utility\HttpTelemetry;
 use Auth0\Symfony\Contracts\ServiceInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Kernel;
 
 final class Service implements ServiceInterface
 {
+    public const VERSION = '5.0.0-BETA1';
+
     private ?Auth0 $sdk = null;
 
     public function __construct(
@@ -27,6 +31,9 @@ final class Service implements ServiceInterface
         if (null === $this->sdk) {
             $this->warmUp();
             $this->sdk = new Auth0($this->configuration);
+
+            HttpTelemetry::setEnvProperty('Symfony', Kernel::VERSION);
+            HttpTelemetry::setPackage('jwt-auth-bundle', self::VERSION);
         }
 
         return $this->sdk;
