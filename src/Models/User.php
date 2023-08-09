adfed7a1bce722f5e365e38ab2135835ed016471
Vincent Sajkowski <57948250+vinnysaj@users.noreply.github.com>
2023-08-09 01:54:58 -0400

    User model fixes (#156)

--- src/Models/User.php
+++ src/Models/User.php
@@ -61,10 +61,10 @@ class User implements SymfonyUserInterface, UserInterface
 
     public function isEmailVerified(): bool
     {
-        return $this->data['email_verified'] ?? false;
+        return filter_var($this->data['email_verified'], FILTER_VALIDATE_BOOLEAN) ?? false;
     }
 
-    public function gePhoneNumber(): ?string
+    public function getPhoneNumber(): ?string
     {
         return $this->data['phone_number'] ?? null;
     }
